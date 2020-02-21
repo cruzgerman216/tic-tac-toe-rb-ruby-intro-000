c15e0f408149f97b98073370692e28bf065850e5
German Cruz <cruzgerman216@yahoo.com>
2020-02-21 19:21:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,8 +12,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index)
-  board[index]
+def move(board, index, current_player)
+  board[index] = current_player
 end
 
 def position_taken?(board, index)
